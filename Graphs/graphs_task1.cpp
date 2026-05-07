051a5174428ee2079c424e7a983609142c81c3e8
Daniil <yuutaprod@gmail.com>
2026-05-07 13:01:30 +0400

    Final version of HW. Added special DFS and BFS functions. Updated txt files for more compability with tasks

--- Graphs/graphs_task1.cpp
+++ Graphs/graphs_task1.cpp
@@ -2,10 +2,22 @@
 #include <fstream>
 #include <map>
 #include <list>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-map<int, list<pair<int, double>>> buildAdjList(int N, int M, ifstream& in, bool orient, bool isWeight){
-    map<int, list<pair<int, double>>> l_Adj;
+
+map<int, list<pair<int, double>>> l_Adj;
+map<int, list<pair<int, double>>> l_AdjT;
+map<int, bool> used;
+map<int, int> pr; 
+vector<int> path, order;
+vector<vector<int>> cycles;
+vector<vector<int>> cycles_sort;
+int N, M;
+bool orient, isWeight;
+
+void buildAdjList(ifstream& in){
     int x, y;
     double w;
 
@@ -30,22 +42,27 @@ map<int, list<pair<int, double>>> buildAdjList(int N, int M, ifstream& in, bool 
         if(orient){
             if(fl){
                 l_Adj[x].push_back(make_pair(y, w));
+                l_AdjT[y].push_back(make_pair(x, w)); // транспонированный
                 if(l_Adj.find(y) == l_Adj.end()){
                     l_Adj[y].push_back(make_pair(0, 0));
                     auto it = l_Adj.find(y);
-                    it -> second.erase(it -> second.begin());
+                    it->second.erase(it->second.begin());
+                }
+                if(l_AdjT.find(x) == l_AdjT.end()){
+                    l_AdjT[x].push_back(make_pair(0, 0));
+                    auto it = l_AdjT.find(x);
+                    it->second.erase(it->second.begin());
                 }
             }
-        } 
+        }
         else if(fl){
             l_Adj[x].push_back(make_pair(y, w));
             l_Adj[y].push_back(make_pair(x, w));
         }
     }
-    return l_Adj;
 }
 
-void printAdjList(map<int, list<pair<int, double>>>& l_Adj, bool isWeight){
+void printAdjList(){
     for(auto it = l_Adj.begin(); it != l_Adj.end(); it++){
         cout << it -> first << ": ";
         if(isWeight){
@@ -62,7 +79,7 @@ void printAdjList(map<int, list<pair<int, double>>>& l_Adj, bool isWeight){
     }
 }
 
-void printUnadjVertex(map<int, list<pair<int, double>>>& l_Adj, int vertex){
+void printUnadjVertex(int vertex){
     for(auto it = l_Adj.begin(); it != l_Adj.end(); it++){
         int v = it -> first;
         if(v == vertex){
@@ -84,7 +101,7 @@ void printUnadjVertex(map<int, list<pair<int, double>>>& l_Adj, int vertex){
     
 }
 
-void insertEdge(map<int, list<pair<int, double>>>& l_Adj, int vertex_1, int vertex_2){
+void insertEdge(int vertex_1, int vertex_2){
     if(vertex_1 == vertex_2){
         cout << "Can't add loop!";
         return;
@@ -103,7 +120,7 @@ void insertEdge(map<int, list<pair<int, double>>>& l_Adj, int vertex_1, int vert
     }
 }
 
-void sdegApproach(map<int, list<pair<int, double>>>& l_Adj, int vertex){
+void sdegApproach(int vertex){
     int k = 0;
     for(auto it = l_Adj.begin(); it != l_Adj.end(); it++){
         int v = it -> first;
@@ -118,51 +135,179 @@ void sdegApproach(map<int, list<pair<int, double>>>& l_Adj, int vertex){
     cout << k << endl;
 }
 
+void DFS(int vertex){
+    used[vertex] = 1;
+    path.push_back(vertex);
+    for (auto it = l_Adj[vertex].begin(); it != l_Adj[vertex].end(); it++){
+        if(!used[it -> first]){
+            DFS(it -> first);
+        }   
+    }
+}
+
+
+void addCycle(int s_vertex, int e_vertex){
+    int cur = e_vertex;
+    vector<int> tmp;
+    while(cur != s_vertex){
+        tmp.push_back(cur);
+        cur = pr[cur];
+    }
+    tmp.push_back(s_vertex);
+    reverse(tmp.begin(), tmp.end());
+    if(tmp.size() < 3){
+
+        return;
+    }
+    cycles.push_back(tmp);
+    sort(tmp.begin(), tmp.end());
+    cycles_sort.push_back(tmp);
+}
+
+void findCycle(int vertex){
+    used[vertex] = 1;
+    for (auto it = l_Adj[vertex].begin(); it != l_Adj[vertex].end(); it++){
+        int v = it -> first;
+        if(pr[v] == vertex){
+            continue;
+        } 
+        if(!used[v]){
+            pr[v] = vertex;
+            findCycle(v);
+        }  
+        else{
+            addCycle(v, vertex);
+        }
+    }
+    used[vertex] = 0;
+}
+
+void dfs1(int vertex){
+    used[vertex] = 1;
+    for (auto it = l_Adj[vertex].begin(); it != l_Adj[vertex].end(); it++){
+        int v = it -> first;
+        if(!used[v]){
+            dfs1(v);
+        }
+    }
+    order.push_back(vertex);
+}
+
+void dfs2(int vertex){
+    used[vertex] = 1;
+    path.push_back(vertex);
+    for (auto it = l_AdjT[vertex].begin(); it != l_AdjT[vertex].end(); it++){
+        int v = it -> first;
+        if(!used[v]){
+            dfs2(v);
+        }
+    }
+}
+
 
 int main(){
     int numTask;
-    int N, M;
-    bool orient, isWeight;
     cout << "What task(1-5) do you want to check? "; cin >> numTask;
 
     if(numTask == 1){
         ifstream in("graph.txt");
         in >> N >> M >> orient >> isWeight;
-        auto l_Adj = buildAdjList(N, M, in, orient, isWeight);
+        buildAdjList(in);
         int vertex;
-        cout << "Insert the vertex you want to find adjacency: "; cin >> vertex;
-        printUnadjVertex(l_Adj, vertex);
+        cout << "Insert the vertex(0 - " << N - 1 <<") you want to find that don't have adjacency: "; cin >> vertex;
+        printUnadjVertex(vertex);
+        cout << endl;
+        printAdjList();
         in.close();
     }
+
     else if(numTask == 2){
         ifstream in("graph_orient.txt");  
         in >> N >> M >> orient >> isWeight;
-        auto l_Adj = buildAdjList(N, M, in, orient, isWeight);
+        buildAdjList(in);
 
         int vertex_A, vertex_B;
         cout << "Insert your vertex A from 0 to " << N - 1<< ": "; cin >> vertex_A; cout << endl;
         cout << "Insert your vertex B from 0 to " << N - 1<< ": "; cin >> vertex_B; cout << endl;
-        printAdjList(l_Adj, isWeight);
+        printAdjList();
         cout << endl;
-        insertEdge(l_Adj, vertex_A, vertex_B);
+        insertEdge(vertex_A, vertex_B);
         cout << endl;
-        printAdjList(l_Adj, isWeight);
+        printAdjList();
         in.close();
     }
+    
     else if(numTask == 3){
         ifstream in("graph_orient.txt");  
         in >> N >> M >> orient >> isWeight;
-        auto l_Adj = buildAdjList(N, M, in, orient, isWeight);
+        buildAdjList(in);
 
         int vertex;
-        cout << "Insert the vertex for which you want to calculate the indegree: "; cin >> vertex;
-        sdegApproach(l_Adj, vertex);
+        cout << "Insert the vertex(0 - " << N - 1<<") for which you want to calculate the indegree: "; cin >> vertex;
+        sdegApproach(vertex);
         cout << endl;
-        printAdjList(l_Adj, isWeight);
+        printAdjList();
         in.close();
     }
+    else if(numTask == 4){
+        ifstream in("graph.txt");  
+        in >> N >> M >> orient >> isWeight;
+        buildAdjList(in);
 
+        for(auto it = l_Adj.begin(); it != l_Adj.end(); it++){
+            pr[it -> first] = -1;
+        }
+        for(auto it = l_Adj.begin(); it != l_Adj.end(); it++){
+            if(!used[it -> first]){
+                findCycle(it -> first);
+            }
+        }
+        for (int i = 0; i < cycles_sort.size(); i++){
+            for (int j = i + 1; j < cycles_sort.size(); j++){
+                if(cycles_sort[i] == cycles_sort[j]){
+                    cycles.erase(cycles.begin() + j);
+                    cycles_sort.erase(cycles_sort.begin() + j);
+                    j--;
+                }
+            }
+        }
+        if (cycles.empty()){
+            cout << "No cycles in graph" << endl;
+        }
+        else{
+            for(auto& c : cycles){
+                for(int v : c){
+                    cout << v << " -> ";
+                }
+                cout << c[0] << endl;
+            }
+        }
+    in.close(); 
+    }
+    else if(numTask == 5){
+        ifstream in("graph_orient.txt");  
+        in >> N >> M >> orient >> isWeight;
+        buildAdjList(in);
+        for(auto it = l_Adj.begin(); it != l_Adj.end(); it++){
+            if(!used[it -> first]){
+                dfs1(it -> first);
+            }
+        
 
-    
+        }
+        used.clear();
+        for(int i = order.size() - 1; i >= 0; i--){
+            int v = order[i];
+            if(!used[v]){
+                path.clear();
+                dfs2(v);
+                for(int c : path){
+                    cout << c << " ";
+                }
+                cout << endl;
+            }
+        }
+        in.close();
+    }
     return 0;
 }
